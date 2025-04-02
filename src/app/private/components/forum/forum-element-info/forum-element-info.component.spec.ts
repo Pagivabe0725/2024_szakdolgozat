6184599a9506f1bb72b5c179be82acbef7a05dcd
Kovács <kovpatrik0725@gmail.com>
2025-04-02 23:38:28 +0200

    update forum-element-info.component.spec.ts

--- src/app/private/components/forum/forum-element-info/forum-element-info.component.spec.ts
+++ src/app/private/components/forum/forum-element-info/forum-element-info.component.spec.ts
@@ -9,6 +9,43 @@ import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Dialog } from '../../../../shared/interfaces/dialog';
 import { of } from 'rxjs';
+import { forum } from '../../../../shared/interfaces/forum';
+import { Timestamp } from '@angular/fire/firestore';
+import { user } from '../../../../shared/interfaces/user';
+
+const dialogTemplate: Dialog = {
+  width: '70%',
+  height: '70%',
+  hostComponent: 'ForumElementInfoComponent',
+  title: '',
+  content: '',
+  action: false,
+  hasInput: false,
+};
+
+const forumTemplate: forum = {
+  title: 'first',
+  id: '1',
+  userId: '1',
+  text: 'It is a test text',
+  author: 'Tester',
+  date: Timestamp.now(),
+  commentsIdArray: [],
+  dislikeArray: [],
+  likeArray: [],
+  category: 'category1',
+};
+
+const userTemplate: user = {
+  id: 'UserId',
+  firstName: 'firstName',
+  lastName: 'lastName',
+  lastLogin: Timestamp.now(),
+  email: 'test@gmail.com',
+  city: undefined,
+  telNumber: '06905777170',
+  dateOfRegistration: Timestamp.now(),
+};
 
 fdescribe('ForumElementInfoComponent', () => {
   let component: ForumElementInfoComponent;
@@ -19,24 +56,12 @@ fdescribe('ForumElementInfoComponent', () => {
   let userServiceMock: jasmine.SpyObj<UserService>;
   //let ActivatedRouteMock: jasmine.SpyObj<ActivatedRoute>;
 
-  const dialogTemplate: Dialog = {
-    width: '70%',
-    height: '70%',
-    hostComponent: 'ForumElementInfoComponent',
-    title: '',
-    content: '',
-    action: false,
-    hasInput: false,
-  };
-
   /*
   private collectionService: CollectionService,
   private popupService: PopupService,
   private navigationService: NavigateAndurlinfoService,
   private arrayService: ArrayService,
   private userService: UserService
-
-
 */
 
   beforeEach(async () => {
@@ -66,7 +91,7 @@ fdescribe('ForumElementInfoComponent', () => {
         { provide: UserService, useValue: userServiceMock },
         {
           provide: ActivatedRoute,
-          useValue: { params: of({ id: 'forumId123' }) },
+          useValue: { params: of({ forumId: 'forumId123' }) },
         },
       ],
     }).compileComponents();
@@ -75,10 +100,74 @@ fdescribe('ForumElementInfoComponent', () => {
 
     fixture = TestBed.createComponent(ForumElementInfoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Before onInit', () => {
+    it('httpSub should be undefined', () => {
+      expect(component['httpSub']).not.toBeDefined();
+    });
+
+    it('actualForumId should be undefined', () => {
+      expect(component['actualForumId']).not.toBeDefined();
+    });
+
+    it('forumSub should be undefined', () => {
+      expect(component['forumSub']).not.toBeDefined();
+    });
+    it('popupSub should be undefined', () => {
+      expect(component['popupSub']).not.toBeDefined();
+    });
+    it('userSub should be undefined', () => {
+      expect(component['userSub']).not.toBeDefined();
+    });
+    it('dialogSub should be undefined', () => {
+      expect(component['dialogSub']).not.toBeDefined();
+    });
+    it('actualForumElement should be undefined', () => {
+      expect(component['actualForumElement']).not.toBeDefined();
+    });
+  });
+
+  describe('Basics', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+
+      collectionServiceMock.getCollectionByCollectionAndDoc.and.returnValue(
+        of({ ...forumTemplate })
+      );
+      userServiceMock.getUserInfoByUserId.and.returnValue(
+        of({ ...userTemplate })
+      );
+      popupServiceMock.getTemplateDialog.and.returnValue({ ...dialogTemplate });
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('httpSub should be defined', () => {
+      expect(component['httpSub']).toBeDefined();
+    });
+
+    it('actualForumId should be defined', () => {
+      expect(component['actualForumId']).toBeDefined();
+    });
+
+    it('forumSub should be defined', async () => {
+      await component.ngOnInit();
+      console.log(component['forumSub']);
+      expect(component['forumSub']).toBeDefined();
+    });
+
+    it('userSub should be defined', async () => {
+      await component.ngOnInit();
+      expect(component['userSub']).toBeDefined();
+    });
+
+    it('actualForumElement should be defined', async () => {
+      await component.ngOnInit();
+      console.log(component['actualForumElement'])
+      expect(component['actualForumElement']).toBeDefined();
+    });
   });
 });
