b0b97230f4e35f6fe16124f4433ad3f0006f31de
Kovács <kovpatrik0725@gmail.com>
2025-04-08 10:54:44 +0200

    updated page test

--- src/app/private/components/page/page.component.spec.ts
+++ src/app/private/components/page/page.component.spec.ts
@@ -2,8 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageComponent } from './page.component';
 import { infoboxInMain_component } from '../../../shared/interfaces/info_board';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const infoBoxTemplate:infoboxInMain_component = {
+const infoBoxTemplate: infoboxInMain_component = {
   icon: 'sensor_door',
   title: 'something',
   text: 'it is the text',
@@ -16,12 +17,12 @@ fdescribe('PageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageComponent],
+      imports: [PageComponent,BrowserAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PageComponent);
     component = fixture.componentInstance;
-  
+
     fixture.detectChanges();
   });
 
@@ -29,12 +30,49 @@ fdescribe('PageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display the correct number of columns (2)', () => {
-    component.color='none';
-    component.columns=2;
-component.actualInfoBoardArray= [{...infoBoxTemplate},{...infoBoxTemplate}]
-    const html:HTMLElement = fixture.nativeElement
-    expect(html.querySelector('.own-page_component-grid-display-2column')).toBeTruthy()
-    console.log(html)
+  it('should display the default number of columns when input data is missing from parent', () => {
+   
+    const html: HTMLElement = fixture.nativeElement;
+    expect(
+      html.querySelector('.own-page_component-grid-display-2column')
+    ).toBeTruthy();
+    console.log(html);
   });
+
+
+  it('should display the correct number of columns (2)', async() => {
+    component.color = 'none';
+    component.columns = 2;
+    component.actualInfoBoardArray = [
+      { ...infoBoxTemplate },
+      { ...infoBoxTemplate },
+      { ...infoBoxTemplate },
+    ];
+    await fixture.detectChanges()
+    const html: HTMLElement = fixture.nativeElement;
+    expect(
+      html.querySelector('.own-page_component-grid-display-2column')
+    ).toBeTruthy();
+    console.log(html);
+  });
+
+
+  it('should display the correct number of columns (3)', async() => {
+    component.color = 'none';
+    component.columns = 3;
+    component.actualInfoBoardArray = [
+      { ...infoBoxTemplate },
+      { ...infoBoxTemplate },
+      { ...infoBoxTemplate },
+    ];
+    await fixture.detectChanges()
+    const html: HTMLElement = fixture.nativeElement;
+    expect(
+      html.querySelector('.own-page_component-grid-display-3column')
+    ).toBeTruthy();
+    console.log(html);
+  });
+
+
+  
 });
