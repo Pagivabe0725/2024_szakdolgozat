7ca70a0faa1ef108137b7736bedc4ee911d582a9
Kovács <kovpatrik0725@gmail.com>
2025-04-08 12:38:06 +0200

    update private component test

--- src/app/private/private.component.spec.ts
+++ src/app/private/private.component.spec.ts
@@ -1,20 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { PrivateComponent } from './private.component';
+import { Routes } from '@angular/router';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { fakeAsync, tick } from '@angular/core/testing';
+import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('PrivateComponent', () => {
+const routes: Routes = [{ path: 'main', component: MainComponent }];
+
+let router: Router;
+let location: Location;
+
+fdescribe('PrivateComponent', () => {
   let component: PrivateComponent;
   let fixture: ComponentFixture<PrivateComponent>;
   let compiled: HTMLElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PrivateComponent],
+      imports: [PrivateComponent, RouterTestingModule.withRoutes(routes),BrowserAnimationsModule],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
 
     fixture = TestBed.createComponent(PrivateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     compiled = fixture.nativeElement;
+
+    router.initialNavigation();
   });
 
   it('should create', () => {
@@ -34,4 +50,22 @@ describe('PrivateComponent', () => {
     );
     expect(element.tagName).toEqual('DIV');
   });
+
+  it('should contain router-outlet', () => {
+    const html: HTMLElement = fixture.nativeElement;
+    const outlet = html.querySelector('router-outlet');
+    expect(outlet).toBeTruthy();
+  });
+
+  it('should load MainComponent on navigating to /main', fakeAsync(() => {
+    router.navigate(['/main']);
+    tick();
+    fixture.detectChanges();
+  
+    const html = fixture.nativeElement as HTMLElement;
+    expect(html.querySelector('app-main')).toBeTruthy();
+    console.log(html)
+  }));
+
+
 });
