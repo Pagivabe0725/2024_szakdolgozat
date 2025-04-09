b2d28cedcef81ad040e3f931f3d9ae2529ae6682
Kovács <kovpatrik0725@gmail.com>
2025-04-09 21:10:45 +0200

    test(routerPlusService):basic setup

--- src/app/shared/services/router-plus.service.spec.ts
+++ src/app/shared/services/router-plus.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RouterPlusService } from './router-plus.service';
+import { Router } from '@angular/router';
 
-describe('RouterPlusService', () => {
+fdescribe('RouterPlusService', () => {
   let service: RouterPlusService;
-
+  let routerMock: jasmine.SpyObj<Router>;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    routerMock = jasmine.createSpyObj(
+      'Router',
+      ['url', 'navigate', 'navigateByUrl'],
+      {
+        url: 'somewhere/someone',
+      }
+    );
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: Router, useValue: routerMock }],
+    });
     service = TestBed.inject(RouterPlusService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('service should be exist',()=>{
+
+    expect(service['router']).toBeDefined()
+
+  })
 });
