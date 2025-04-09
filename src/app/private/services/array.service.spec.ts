311ae49136e690f1c36053e85c9ceb2e8f07e8c2
Kovács <kovpatrik0725@gmail.com>
2025-04-09 10:13:23 +0200

    test(arrayService):getIndex

--- src/app/private/services/array.service.spec.ts
+++ src/app/private/services/array.service.spec.ts
@@ -2,21 +2,22 @@ import { TestBed } from '@angular/core/testing';
 
 import { ArrayService } from './array.service';
 
-function randomNumber(num:number): number {
+function randomNumber(num: number): number {
   return Math.floor(Math.random() * num);
 }
 
 fdescribe('ArrayService', () => {
   let service: ArrayService;
-  let testArray:string[]
-  let randomNum:number
+  let testArray: string[];
+  let randomNum: number;
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ArrayService);
-    randomNum=randomNumber(100);
-    testArray=[]
+    randomNum = randomNumber(100);
+    testArray = [];
     for (let i = 0; i < randomNum; i++) {
-     testArray.push('') 
+      testArray.push(' ');
     }
   });
 
@@ -24,9 +25,22 @@ fdescribe('ArrayService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('getIndex should work', () => {
-    console.log(testArray)
 
+  describe('getIndex',()=>{
 
-  });
+    it('getIndex should return index if element in array', () => {
+      const localRandomNum = randomNumber(randomNum);
+      testArray[localRandomNum] = 'find me';
+      const result = service.getIndex('find me', testArray);
+      console.log(result);
+      expect(localRandomNum).toBe(result);
+    });
+  
+    it('getIndex should return 0 if element not in array', () => {
+      console.log(testArray)
+      const result = service.getIndex('find me', testArray);
+      expect(result).toBe(testArray.length);
+    });
+  })
+ 
 });
