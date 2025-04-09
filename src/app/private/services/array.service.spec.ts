9c00d432b6a30f558d165f741154e4e46bbc33f6
Kovács <kovpatrik0725@gmail.com>
2025-04-09 09:59:22 +0200

    test(arrayService): started

--- src/app/private/services/array.service.spec.ts
+++ src/app/private/services/array.service.spec.ts
@@ -2,15 +2,31 @@ import { TestBed } from '@angular/core/testing';
 
 import { ArrayService } from './array.service';
 
-describe('ArrayService', () => {
-  let service: ArrayService;
+function randomNumber(num:number): number {
+  return Math.floor(Math.random() * num);
+}
 
+fdescribe('ArrayService', () => {
+  let service: ArrayService;
+  let testArray:string[]
+  let randomNum:number
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ArrayService);
+    randomNum=randomNumber(100);
+    testArray=[]
+    for (let i = 0; i < randomNum; i++) {
+     testArray.push('') 
+    }
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getIndex should work', () => {
+    console.log(testArray)
+
+
   });
 });
