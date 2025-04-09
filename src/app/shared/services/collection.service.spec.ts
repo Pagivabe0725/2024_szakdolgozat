0dd43c4b4f026379534adacdc4099c1bac315c26
Kovács <kovpatrik0725@gmail.com>
2025-04-09 20:56:11 +0200

    test(collectionService)

--- src/app/shared/services/collection.service.spec.ts
+++ src/app/shared/services/collection.service.spec.ts
@@ -1,29 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CollectionService } from './collection.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { of } from 'rxjs';
 
 fdescribe('CollectionService', () => {
   let service: CollectionService;
-  const firestoreMock = {
-    collection: jasmine.createSpy().and.callFake((collectionName: string) => ({
-      doc: jasmine.createSpy().and.callFake((docId: string) => ({
-        valueChanges: jasmine
-          .createSpy()
-          .and.returnValue(of({ data: 'mockData' })),
-        set: jasmine.createSpy().and.returnValue(Promise.resolve()),
-        delete: jasmine.createSpy().and.returnValue(Promise.resolve()),
-      })),
-      get: jasmine
-        .createSpy()
-        .and.returnValue(of([{ id: 'doc1' }, { id: 'doc2' }])),
-    })),
-  };
+
+  let valueChangesSpy: jasmine.Spy;
+  let setSpy: jasmine.Spy;
+  let deleteSpy: jasmine.Spy;
+  let getSpy: jasmine.Spy;
+  let docSpy: jasmine.Spy;
+  let collectionSpy: jasmine.Spy;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [{ provide: AngularFirestore, useValue: firestoreMock }],
+    valueChangesSpy = jasmine.createSpy().and.returnValue(of({ data: 'mockData' }));
+    setSpy = jasmine.createSpy().and.returnValue(Promise.resolve('setDone'));
+    deleteSpy = jasmine.createSpy().and.returnValue(Promise.resolve('deleteDone'));
+    getSpy = jasmine.createSpy().and.returnValue(of([{ id: 'doc1' }, { id: 'doc2' }]));
+    docSpy = jasmine.createSpy().and.returnValue({
+      valueChanges: valueChangesSpy,
+      set: setSpy,
+      delete: deleteSpy
     });
+    collectionSpy = jasmine.createSpy().and.returnValue({
+      doc: docSpy,
+      get: getSpy
+    });
+
+    const firestoreMock = {
+      collection: collectionSpy
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AngularFirestore, useValue: firestoreMock }
+      ]
+    });
+
     service = TestBed.inject(CollectionService);
   });
 
@@ -31,12 +45,45 @@ fdescribe('CollectionService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should return valueChanges observable from getCollectionByCollectionAndDoc', (done) => {
+    service.getCollectionByCollectionAndDoc('testCollection', 'testDoc').subscribe((data) => {
+      expect(data).toEqual({ data: 'mockData' });
+      expect(collectionSpy).toHaveBeenCalledWith('testCollection');
+      expect(docSpy).toHaveBeenCalledWith('testDoc');
+      expect(valueChangesSpy).toHaveBeenCalled();
+      done();
+    });
+  });
 
-  it('should execute constructor before any other logic',()=>{
-    expect(service['angularFirestore']).toBeDefined()
-  })
+  it('should return documents from getAllDocByCollectionName', (done) => {
+    service.getAllDocByCollectionName('testCollection').subscribe((docs) => {
+      expect(docs).toEqual([{ id: 'doc1' }, { id: 'doc2' }]);
+      expect(collectionSpy).toHaveBeenCalledWith('testCollection');
+      expect(getSpy).toHaveBeenCalled();
+      done();
+    });
+  });
 
-  describe('Functions:',()=>{
-    
-  })
+  it('should call set method in createCollectionDoc', async () => {
+    const data = { title: 'testData' };
+    await service.createCollectionDoc('testCollection', 'testId', data);
+    expect(collectionSpy).toHaveBeenCalledWith('testCollection');
+    expect(docSpy).toHaveBeenCalledWith('testId');
+    expect(setSpy).toHaveBeenCalledWith(data);
+  });
+
+  it('should call set method in updateDatas', async () => {
+    const update = { updated: true };
+    await service.updateDatas('testCollection', 'testDoc', update);
+    expect(collectionSpy).toHaveBeenCalledWith('testCollection');
+    expect(docSpy).toHaveBeenCalledWith('testDoc');
+    expect(setSpy).toHaveBeenCalledWith(update);
+  });
+
+  it('should call delete method in deleteDatas', async () => {
+    await service.deleteDatas('testCollection', 'testDoc');
+    expect(collectionSpy).toHaveBeenCalledWith('testCollection');
+    expect(docSpy).toHaveBeenCalledWith('testDoc');
+    expect(deleteSpy).toHaveBeenCalled();
+  });
 });
